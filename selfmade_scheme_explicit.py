8ec6abd2e29a1b24665a5aa27b65bbcb343bae70
chrisbso <chrisbso@stud.ntnu.no>
2018-03-29 14:14:11 +0200

    Some more hiccups. Added the files for homebrew.

---
+++ selfmade_scheme_explicit.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+plt.style.use("ggplot")
+
+N = 2000
+M = 100
+dx = 10 / (M+1)
+T = 10
+dt = T / (N + 1)
+g = 10
+
+xvalues = np.array([i * dx for i in range(M+2)])
+tvalues = np.array([i * dt for i in range(N+2)])
+
+h = np.array([[1.] * (M + 2)] * (N+2))
+u = np.array([[0.] * (M + 2)] * (N+2))
+
+h[0,0:M+2] = 1+2/5*np.exp(-5*(xvalues-dx*(M+1)/2)**2)
+
+
+
+
+
+def ddx(y,n, m):
+    return (y[n][m+1] - y[n][m - 1]) / (2*dx)
+    #return (3*y[n][m] - 4*y[n][m-1] + y[n][m-2]) / (2*dx)
+
+
+
+def main():
+
+    for n in range(N+1):
+        for m in range(1,M+1):
+            h[n+1][m] = max(0, -(ddx(h,n,m)*u[n][m]+h[n][m]*ddx(u,n,m))*dt+h[n][m])
+            if h[n+1][m] == 0:
+                u[n+1][m] = 0
+            else:
+                u[n+1][m] = (-u[n][m]*(h[n+1][m]-h[n][m])/dt-2*h[n][m]*u[n][m]*ddx(u,n,m)-u[n][m]**2*ddx(h,n,m)-g*h[n][m]*ddx(h,n,m))*dt/h[n][m]+u[n][m]
+        #h[n+1][0] = 8/10*h[n+1][1]-2/10*h[n+1][2]
+        #h[n + 1][M+1] = 8/10 * h[n + 1][M] - 2/10*h[n + 1][M-1]
+        #Uncomment these to remove boundaries forced to 1
+    fig, ax = plt.subplots()
+
+    line, = ax.plot(xvalues, h[0])
+
+    def animate(i):
+        line.set_ydata(h[i])
+        return line,
+
+    def init():
+        line.set_ydata(np.ma.array(xvalues, mask=True))
+        return line,
+
+    ax.ani = animation.FuncAnimation(fig, animate, np.arange(1, N+2), init_func=init,
+                                     interval=25, blit=True)
+    plt.ylim(0, 3)
+    plt.show()
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
---
+++ selfmade_scheme_implicit.py
@@ -0,0 +1,82 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+plt.style.use("ggplot")
+
+N = 500
+M = 150
+dx = 10 / (M+1)
+T = 10
+dt = T / (N + 1)
+g = 1
+
+xvalues = np.array([i * dx for i in range(M+2)])
+tvalues = np.array([i * dt for i in range(N+2)])
+
+h = np.array([[1.] * (M + 2)] * (N+2))
+u = np.array([[0.] * (M + 2)] * (N+2))
+
+h[0,0:M+2] = 1+2/5*np.exp(-5*(xvalues-dx*(M+1)/2)**2)
+
+
+
+
+
+def ddx(y,n, m):
+    return (y[n][m+1] - y[n][m - 1]) / (2*dx)
+    #return (3*y[n][m] - 4*y[n][m-1] + y[n][m-2]) / (2*dx)
+
+
+
+def main():
+    A = np.array([[0.] * (2*M)] * 2*M)
+    b = np.array([0.] * (2*M))
+    for n in range(N+1):
+        for i in range(1,M-1):
+            A[(2*(i-1)):2*(i-1)+6,i] = np.array([dt*h[n][i],dt*u[n][i]*h[n][i],0,2*dx*h[n][i+1],-dt*h[n][i+2],-dt*u[n][i+2]*h[n][i+2]])
+        for i in range(M+1,2*M-1):
+            A[(2*(i-M-1)):2*(i-M-1)+6,i] = np.array([dt*u[n][i-M],dt*g*h[n][i-M],2*dx,0,-dt*u[n][i+2-M],-dt*g*h[n][i+2-M]])
+        A[1:4,0]                = np.array([2*dx*h[n][1],-dt*h[n][2],-dt*u[n][2]*h[n][2]])
+        A[2*M-4:2*M,M-1]        = np.array([dt*h[n][M-1],dt*u[n][M-1]*h[n][M-1],0,2*dx*h[n][M]])
+        A[0:4, M]               = np.array([2*dx,0,-dt*u[n][2],-dt*g*h[n][2]])
+        A[2 * M - 4:2 * M, 2*M -1] = np.array([dt*u[n][M-1],dt*g*h[n][M-1],2*dx,0])
+
+        for i in range(0,2*M,2):
+            b[i]    =  2*dx*h[n][i//2+1]
+            b[i+1]  =  2*dx*h[n][i//2+1]*u[n][i//2+1]
+        b[0]    += dt*u[n][1]
+        b[1]    += dt*g*h[n][1]
+        b[-2]   += -dt*u[n][M]
+        b[-1]   += -dt*g*h[n][M]
+
+        x = np.linalg.lstsq(A,b,rcond=None)
+        h[n+1][1:M+1] = x[0][M:2*M]
+        u[n+1][1:M+1] = x[0][0:M]
+
+    fig, ax = plt.subplots()
+
+    line, = ax.plot(xvalues, h[0])
+
+    def animate(i):
+        line.set_ydata(h[i])
+        return line,
+
+    def init():
+        line.set_ydata(np.ma.array(xvalues, mask=True))
+        return line,
+
+    ax.ani = animation.FuncAnimation(fig, animate, np.arange(1, N+2), init_func=init,
+                                     interval=25, blit=True)
+    plt.ylim(0, 3)
+    plt.show()
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
